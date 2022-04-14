468f3a1eb633c062fae2de65f1f13d7ba04325b3
Mariem KAMMOUN <mariem.kammoun@actia-engineering.tn>
2022-04-14 14:09:58 +0100

    First commit

---
+++ src/main/java/com/Mariem/GradleTutorial.java
@@ -0,0 +1,7 @@
+package com.Mariem;
+
+public class GradleTutorial {
+    public static void main(String[] args) {
+        System.out.println("Gradle Hello World!");
+    }
+}
---
+++ src/test/java/com/Mariem/GradleTutorialTest.java
@@ -0,0 +1,12 @@
+package com.Mariem;
+
+import org.junit.Test;
+
+public class GradleTutorialTest {
+    
+    @Test
+    public void verifyNoExceptionThrown() {
+        GradleTutorial.main(new String[]{});
+    }
+
+}
